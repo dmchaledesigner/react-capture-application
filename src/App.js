94e1422960bab137c4db3645f7985cff0e572db4
david mchale <dmchaledesigner@gmail.com>
2021-06-27 14:12:38 +1000

    4. intro to styled components

--- src/App.js
+++ src/App.js
@@ -4,9 +4,18 @@ import React from 'react';
 import AboutUsPage from './Pages/AboutUsPage';
 
 
+
+
+//global styles
+import GlobalStyle from './components/GlobalStyle';
+
+
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
+
+
 
       <AboutUsPage />
 
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,37 +1,94 @@
 import React from 'react';
-import { styled } from "styled-components";
 import home1 from '../img/home1.png'; // DO NOT use curly braces as iamges are NOT a named export like from an exported function - its only a filetype
 
 
 
+// styled import
+import styled from "styled-components";
+
+
+
+
 const AboutSection = () => {
+
+
+
     return (
-        <div>
-            <div className="description">
+        <AboutStyled>
+            <DescriptionStyled>
                 <div className="title">
-                    <div className="hide">
+
+                    <HideStyled>
                         <h2>We work to make</h2>
-                    </div>
+                    </HideStyled>
 
-                    <div className="hide">
+                    <HideStyled>
                         <h2>your <span>dreams</span> come</h2>
-                    </div>
+                    </HideStyled>
 
-                    <div className="hide">
+                    <HideStyled>
                         <h2>true</h2>
-                    </div>
+                    </HideStyled>
                 </div>
 
                 <p>Contact us for any photography or any videography ideas that you have</p>
                 <button>Contact Us</button>
-            </div>
+            </DescriptionStyled>
 
-            <div className="image">
+            <ImageStyled>
                 <img src={home1} alt="hero image guy" />
-            </div>
+            </ImageStyled>
 
-        </div>
+        </AboutStyled>
     )
 }
+
+
+
+
+// styled Components
+const AboutStyled = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: #fff;
+
+        h2{
+            color: white;
+        }
+`;
+
+
+const DescriptionStyled = styled.div`
+    flex: 1;
+    padding-right: 5rem;
+
+        h2{
+            font-weight: lighter;
+        }
+`;
+
+
+const HideStyled = styled.div`
+    overflow: hidden;
+`;
+
+
+const ImageStyled = styled.div`
+        flex: 1;
+        overflow:hidden;
+
+            img{
+                width: 100%;
+                height: 80vh;
+                object-fit: cover;
+            }
+`;
+
+
+
+
 
 export default AboutSection
